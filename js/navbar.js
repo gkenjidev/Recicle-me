bd15de823adc8c564584475837efd531cb6c2e58
João Oliveira <joaovictor.oliveira290502@gmail.com>
2022-05-18 01:06:57 -0300

    ESSE TA FODA

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+document.addEventListener("scroll", (event) => {
+    const firstContent = document.querySelector(".navbarscroll");
+    const headerHeight = firstContent.offsetTop;
+
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > headerHeight) {
+        document.querySelector(".navbar-animation").style = "position: fixed; background: linear-gradient(180deg, rgba(0, 56, 5, 0.35) 0%, rgba(15, 186, 29, 0) 149%);; backdrop-filter: blur(22px);"
+        
+    } else {
+        document.querySelector(".navbar-animation").style = "position: absolute;";
+        
+    }
+});
